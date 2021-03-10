d7ad39e3bd3aa2f9d18ba7a3751c7673267bce24
massiliatazit <massiliatazit@users.noreply.github.com>
2021-03-11 00:41:26 +0100

    initial server settings

---
+++ printerest-clone/src/index.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const users = require("./users")
+const pins = require("./pins")
+module.exports = router;
---
+++ printerest-clone/src/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const {join} = require('path')
+const server = express()
+// const http = require('http')
+// const httpServer = http.createServer(server)
+const serviceRouters = require('./services')
+const passport = require("passport");
+const { notFoundHandler, forbiddenHandler, badRequestHandler, genericErrorHandler } = require("./errorHandlers");
+const port = process.env.PORT ||7000 
+const staticFolderPath = join(_dirname, '../public')
+
+server.use(express.static(staticFolderPath))
+server.use(express.json())
+server.use(cors());
+
+server.use(passport.initialize());
+server.use("/", serviceRouters);
+server.use(badRequestHandler);
+server.use(notFoundHandler);
+server.use(forbiddenHandler);
+server.use(genericErrorHandler);
+mongoose.connect(process.env.MONGO_CONNECTION,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+}).then(server.listen(port,()=>{
+    console.log("Running on port", port,)
+})).catch(err=>console.log(err))
